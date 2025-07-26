1f0e44d50fdd8b769069ac29b120dce8ec9902db
ARYAN <aryan.70921@gmail.com>
2025-07-26 12:42:43 +0530

    deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,19 @@
-import express from "express"
-import notesRotes from "./routes/noteRouter.js"
-import connectDb from "./lib/db.js"
+import express from "express";
+import notesRotes from "./routes/noteRouter.js";
+import connectDb from "./lib/db.js";
+import path from 'path';
 const app = express()
 const PORT = 8000
 
-app.use("/api/notes", notesRotes)
+const __dirname=path.resolve();
+app.use("/api/notes", notesRotes);
 
+if(process.env.NODE_URI==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+}
 
 app.listen(PORT, ()=>{
     connectDb();
