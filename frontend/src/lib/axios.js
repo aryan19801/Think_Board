7478a9c395ee8a4288424e7f1c641b1f41d84567
ARYAN <aryan.70921@gmail.com>
2026-05-30 17:13:16 +0530

    fix changes of upstash

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
+
 const api = axios.create({
-    baseURL : "https://think-board-w2ye.onrender.com/api"
+    baseURL: "http://localhost:5001/api"
 });
+
 export default api;
